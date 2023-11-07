ce5318b42e49560135e3e90b86b3e7e1f48909f3
Fredi Kats <fredikats@outlook.com>
2023-11-07 08:54:33 +0100

    Add SharpCompilationProviderBuilder

---
+++ Sources/Kysect.PowerShellRunner.CodeGeneration/Compilation/SharpCompilationProviderBuilder.cs
@@ -0,0 +1,53 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+
+namespace Kysect.PowerShellRunner.CodeGeneration.Compilation;
+
+public class SharpCompilationProviderBuilder
+{
+    private static readonly string StandardLibraryAssemblyPath = typeof(object).Assembly.Location;
+    private static readonly string AssemblyPath = Path.GetDirectoryName(typeof(object).Assembly.Location);
+
+    private CSharpCompilation _compilation;
+
+    public SharpCompilationProviderBuilder(CSharpCompilation compilation)
+    {
+        _compilation = compilation;
+    }
+
+    public static SharpCompilationProviderBuilder CreateForStandard(string name)
+    {
+        return new SharpCompilationProviderBuilder(CSharpCompilation.Create(name))
+            .AddReferences(StandardLibraryAssemblyPath)
+            .AddReferences(Path.Combine(AssemblyPath, "System.dll"))
+            .AddReferences(Path.Combine(AssemblyPath, "System.Collections.dll"))
+            //.AddReferences(MetadataReference.CreateFromFile(Path.Combine(AssemblyPath, "System.Core.dll")))
+            .AddReferences(Path.Combine(AssemblyPath, "System.Linq.dll"))
+            .AddReferences(Path.Combine(AssemblyPath, "System.Linq.Queryable.dll"))
+            .AddReferences(Path.Combine(AssemblyPath, "System.Linq.Expressions.dll"))
+            .AddReferences(Path.Combine(AssemblyPath, "System.Runtime.dll"));
+    }
+
+    public SharpCompilationProviderBuilder AddReferences(Assembly assembly)
+    {
+        assembly.ThrowIfNull();
+
+        return AddReferences(assembly.Location);
+    }
+
+    public SharpCompilationProviderBuilder AddReferences(string assemblyFilePath)
+    {
+        assemblyFilePath.ThrowIfNull();
+
+        _compilation = _compilation.AddReferences(MetadataReference.CreateFromFile(assemblyFilePath));
+        return this;
+    }
+
+    public CSharpCompilation Build()
+    {
+        return _compilation;
+    }
+}
