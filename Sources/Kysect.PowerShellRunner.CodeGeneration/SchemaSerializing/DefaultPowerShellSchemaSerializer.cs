4ad1b4ae3065aa4e1be0e47d3506cda4112e402e
Fredi Kats <fredikats@outlook.com>
2023-11-13 21:17:39 +0100

    Implement schema serializer

---
+++ Sources/Kysect.PowerShellRunner.CodeGeneration/SchemaSerializing/DefaultPowerShellSchemaSerializer.cs
@@ -0,0 +1,66 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Kysect.PowerShellRunner.CodeGeneration.Common.Semantic;
+using Kysect.PowerShellRunner.CodeGeneration.SchemaGenerating;
+using Kysect.PowerShellRunner.CodeGeneration.SemanticParsing;
+using Kysect.PowerShellRunner.CodeGeneration.UsedModelSearching;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kysect.PowerShellRunner.CodeGeneration.SchemaSerializing;
+
+public class DefaultPowerShellSchemaSerializer : IPowerShellSchemaSerializer<CmdletBaseSemanticInfo>
+{
+    public PowerShellSchemaDto ConvertSchemaToSerializable(PowerShellSemanticSchema<CmdletBaseSemanticInfo> schema)
+    {
+        schema.ThrowIfNull();
+
+        IReadOnlyCollection<PowerShellCmdletDescriptor> cmdletDescriptors = schema
+            .CmdletDescriptors
+            .Select(ConvertCmdletToSerializable)
+            .ToList();
+
+        var models = schema
+            .Models
+            .Select(ConvertModelToSerializable)
+            .ToList();
+
+        return new PowerShellSchemaDto(cmdletDescriptors, models, schema.Enums);
+    }
+
+    public PowerShellCmdletDescriptor ConvertCmdletToSerializable(CmdletBaseSemanticInfo semanticInfo)
+    {
+        semanticInfo.ThrowIfNull();
+
+        IReadOnlyCollection<SimpleModelPropertyDescriptor> properties = WrapProperties(semanticInfo.Properties);
+
+        string? originalReturnTypes = semanticInfo.MainReturnType?.GetNameWithContainingParent();
+        var resolvedReturnTypes = semanticInfo.ResolvedReturnTypes.Select(t => t.GetNameWithContainingParent()).ToList();
+
+        // TODO: allow customization
+        IReadOnlyList<string> subScopeNames = new[] { "Cmdlets" };
+        return new PowerShellCmdletDescriptor(
+            subScopeNames,
+            semanticInfo.CmdletAttributeValues,
+            properties,
+            originalReturnTypes,
+            resolvedReturnTypes);
+    }
+
+    public SimpleModelDescriptor ConvertModelToSerializable(RoslynSimpleModelSemanticDescriptor model)
+    {
+        model.ThrowIfNull();
+
+        return new SimpleModelDescriptor(
+            model.Name,
+            model.BaseType?.GetNameWithContainingParent(),
+            WrapProperties(model.PublicProperties),
+            WrapProperties(model.NonPublicProperties));
+    }
+
+    public static IReadOnlyCollection<SimpleModelPropertyDescriptor> WrapProperties(IReadOnlyCollection<RoslynPropertySymbolWrapper> properties)
+    {
+        return properties
+            .Select(p => new SimpleModelPropertyDescriptor(p.TypeName, p.Name))
+            .ToList();
+    }
+}
---
+++ Sources/Kysect.PowerShellRunner.CodeGeneration/SchemaSerializing/IPowerShellSchemaSerializer.cs
@@ -0,0 +1,10 @@
+﻿using Kysect.PowerShellRunner.CodeGeneration.SchemaGenerating;
+using Kysect.PowerShellRunner.CodeGeneration.SemanticParsing;
+
+namespace Kysect.PowerShellRunner.CodeGeneration.SchemaSerializing;
+
+public interface IPowerShellSchemaSerializer<TSemantic> where TSemantic : CmdletBaseSemanticInfo
+{
+    PowerShellSchemaDto ConvertSchemaToSerializable(PowerShellSemanticSchema<TSemantic> schema);
+    PowerShellCmdletDescriptor ConvertCmdletToSerializable(TSemantic semanticInfo);
+}
