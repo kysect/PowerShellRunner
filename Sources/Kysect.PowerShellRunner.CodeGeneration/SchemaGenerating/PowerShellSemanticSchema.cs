3152e15176dcc3f99dbc4e31e7de9f567b059b2f
Fredi Kats <fredikats@outlook.com>
2023-11-13 21:05:41 +0100

    Implement PowerShell semantic schema generating

---
+++ Sources/Kysect.PowerShellRunner.CodeGeneration/SchemaGenerating/PowerShellSemanticSchema.cs
@@ -0,0 +1,33 @@
+﻿using Kysect.PowerShellRunner.CodeGeneration.SemanticParsing;
+using Kysect.PowerShellRunner.CodeGeneration.UsedModelSearching;
+using System.Collections.Generic;
+
+namespace Kysect.PowerShellRunner.CodeGeneration.SchemaGenerating;
+
+public class PowerShellSemanticSchema : PowerShellSemanticSchema<CmdletBaseSemanticInfo>
+{
+    public PowerShellSemanticSchema(
+        IReadOnlyCollection<CmdletBaseSemanticInfo> cmdletDescriptors,
+        IReadOnlyCollection<RoslynSimpleModelSemanticDescriptor> models,
+        IReadOnlyCollection<ModelEnumTypeDescriptor> enums)
+        : base(cmdletDescriptors, models, enums)
+    {
+    }
+}
+
+public class PowerShellSemanticSchema<T> where T : CmdletBaseSemanticInfo
+{
+    public IReadOnlyCollection<T> CmdletDescriptors { get; set; }
+    public IReadOnlyCollection<RoslynSimpleModelSemanticDescriptor> Models { get; set; }
+    public IReadOnlyCollection<ModelEnumTypeDescriptor> Enums { get; }
+
+    public PowerShellSemanticSchema(
+        IReadOnlyCollection<T> cmdletDescriptors,
+        IReadOnlyCollection<RoslynSimpleModelSemanticDescriptor> models,
+        IReadOnlyCollection<ModelEnumTypeDescriptor> enums)
+    {
+        CmdletDescriptors = cmdletDescriptors;
+        Models = models;
+        Enums = enums;
+    }
+}
