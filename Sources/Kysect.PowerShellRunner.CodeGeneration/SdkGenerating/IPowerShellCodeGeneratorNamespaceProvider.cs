d7c814ea212ada840bd866bc605798135729d2e7
Fredi Kats <fredikats@outlook.com>
2023-11-14 20:19:16 +0100

    Implement SDK generating

---
+++ Sources/Kysect.PowerShellRunner.CodeGeneration/SdkGenerating/IPowerShellCodeGeneratorNamespaceProvider.cs
@@ -0,0 +1,11 @@
+﻿using Kysect.PowerShellRunner.CodeGeneration.SchemaGenerating;
+using Kysect.PowerShellRunner.CodeGeneration.UsedModelSearching;
+
+namespace Kysect.PowerShellRunner.CodeGeneration.SdkGenerating;
+
+public interface IPowerShellCodeGeneratorNamespaceProvider
+{
+    string[] GetForEnum(ModelEnumTypeDescriptor enumTypeDescriptor);
+    string[] GetForModel(SimpleModelDescriptor simpleModelDescriptor);
+    string[] GetForCmdlet(PowerShellCmdletDescriptor cmdletDescriptor);
+}
---
+++ Sources/Kysect.PowerShellRunner.CodeGeneration/SdkGenerating/PropertyDeclarationSyntaxGenerationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Kysect.PowerShellRunner.CodeGeneration.SdkGenerating;
+
+public static class PropertyDeclarationSyntaxGenerationExtensions
+{
+    public static PropertyDeclarationSyntax WithGetSetAccessorList(this PropertyDeclarationSyntax propertyDeclaration)
+    {
+        propertyDeclaration.ThrowIfNull();
+
+        return propertyDeclaration
+            .WithAccessorList(
+                SyntaxFactory.AccessorList(
+                    SyntaxFactory.List(
+                        new[]
+                        {
+                            SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+                                .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
+                            SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+                                .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken))
+                        })));
+    }
+}
