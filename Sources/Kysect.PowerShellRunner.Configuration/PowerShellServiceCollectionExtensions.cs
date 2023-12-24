b14a92aa11435d450688664479c75b4422b38ae3
Fredi Kats <fredikats@outlook.com>
2023-12-25 00:38:48 +0100

    Add PowerShellRunner.Configuration for ServiceCollection extensions

---
+++ Sources/Kysect.PowerShellRunner.Configuration/PowerShellServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Kysect.CommonLib.BaseTypes.Extensions;
+using Kysect.CommonLib.DependencyInjection.Logging;
+using Kysect.PowerShellRunner.Abstractions.Accessors;
+using Kysect.PowerShellRunner.Accessors;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Kysect.PowerShellRunner.Configuration;
+
+public static class PowerShellServiceCollectionExtensions
+{
+    public static IServiceCollection AddPowerShellLogger(this IServiceCollection serviceCollection, Action<LogConfigurationBuilder> configurationAction)
+    {
+        serviceCollection.ThrowIfNull();
+        configurationAction.ThrowIfNull();
+
+        using var logConfigurationBuilder = new LogConfigurationBuilder();
+        configurationAction(logConfigurationBuilder);
+        var powerShellLogger = new PowerShellLogger(logConfigurationBuilder.Build());
+        return serviceCollection.AddSingleton<IPowerShellLogger>(powerShellLogger);
+    }
+
+    public static IServiceCollection AddPowerShellAccessorFactory(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection.AddSingleton<IPowerShellAccessorFactory, PowerShellAccessorFactory>();
+    }
+
+    public static IServiceCollection AddPowerShellAccessor(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection.AddSingleton(GetPowerShellAccessor);
+    }
+
+    private static IPowerShellAccessor GetPowerShellAccessor(IServiceProvider serviceProvider)
+    {
+        IPowerShellLogger powerShellLogger = serviceProvider.GetRequiredService<IPowerShellLogger>();
+        IPowerShellAccessorFactory powerShellAccessorFactory = serviceProvider.GetRequiredService<IPowerShellAccessorFactory>();
+
+        IPowerShellAccessor basePowerShellAccessor = powerShellAccessorFactory.Create();
+        var basePowerShellAccessorLogDecorator = new PowerShellAccessorLoggingDecorator(basePowerShellAccessor, powerShellLogger);
+
+        return basePowerShellAccessorLogDecorator;
+    }
+}
