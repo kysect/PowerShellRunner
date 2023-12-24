10a7b76dbbc84ffb63b53f5bf84b46def09674d3
Fredi Kats <fredikats@outlook.com>
2023-12-25 00:38:48 +0100

    Add PowerShellRunner.Configuration for ServiceCollection extensions

---
+++ Sources/Kysect.PowerShellRunner.Configuration/IPowerShellLogger.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Kysect.PowerShellRunner.Configuration;
+
+public interface IPowerShellLogger : ILogger
+{
+}
+
+// TODO: this used for registration more than one logger. dotnet8 provide "named" registration that solve this problem
+public class PowerShellLogger : IPowerShellLogger
+{
+    private readonly ILogger _logger;
+
+    public PowerShellLogger(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        _logger.Log(logLevel, eventId, state, exception, formatter);
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return _logger.IsEnabled(logLevel);
+    }
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        return _logger.BeginScope(state);
+    }
+}
